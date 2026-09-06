f3d5331fc2bfde683043227933b48736630644ac
Ajay Kumar <ajaybhardwaj2542002@gmail.com>
2026-09-06 19:33:36 +0530

    created the ecnounter table and have changed the header in the uppder case of standard data practice

---
+++ healthcare_dbt/models/staging/stg_encounters.sql
@@ -0,0 +1,20 @@
+{{config(MATERIALIZED = 'table')}}
+
+SELECT
+    
+    "Id"                            as ENCOUNTER_ID,
+    "START"::date                   as START_DATE,
+    "STOP"::date                    as END_DATE,
+    'PATIENT'                       as PATIENT_ID,
+    "ORGANIZATION"                  as ORGANIZATION_ID,
+    "PROVIDER"                      as PROVIDER_ID,
+    "PAYER"                         as  PAYER_ID,
+    "ENCOUNTERCLASS"                as  ENCOUNTER_CLASS,
+    "CODE"                          as  CODE_CODE,
+    "DESCRIPTION"                   as  ENCOUNTER_DESCRIPTION,
+    "BASE_ENCOUNTER_COST"::double   as BASE_ENCOUNTER_COST, 
+    "TOTAL_CLAIM_COST" ::double     as  TOTAL_CLAIM_COST,
+    "PAYER_COVERAGE"::double        as  PAYER_COVERAGE,
+    "REASONCODE"                    as  REASON_CODE,
+    "REASONDESCRIPTION"               as   REASON_DESCRIPTION
+from {{source('source','encounters')}}
